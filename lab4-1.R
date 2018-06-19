da572b08320255ee458778d299c6c2b6002b0b03
Anders Swanson <anderssw@metia.com>
2018-06-18 19:57:56 -0700

    finished lab 4-1

--- lab4-1.R
+++ lab4-1.R
@@ -20,22 +20,56 @@ options(dplyr.width = Inf) # shows all columns of a tbl_df object
 
 fname <- list(
     input = 'data/mht_lab2.xdf',
-    stage = 'stage/mht_lab2.xdf'
+    
+    stage = 'stage/mht_lab2.xdf',
+    redo = 'stage/mht_lab2_2.xdf'
 )
 
 mht <- fname %>% map(RxXdfData)
 
 rxSummary(~ tip_percent, mht$input)
 
+# SECTION 1
 
-# Q1 & Q2 -----------------------------------------------------------------
+## Q1 & Q2 -----------------------------------------------------------------
 
 model <- list(
     one = "tip_percent ~ trip_duration + pickup_dow:pickup_hour",
     two = "tip_percent ~ payment_type_desc + trip_duration + pickup_dow:pickup_hour"
     ) %>%
     map(as.formula) %>%
-    map(rxLinMod, data = mht$input)
+    map(rxLinMod, data = mht$input, dropFirst = TRUE, covCoef = TRUE)
 
 map(model, "adj.r.squared")
 
+# SECTION 2
+
+
+pred <- rxPredict(model$one, mht$input, outData = mht$stage,
+                  writeModelVars = TRUE,
+                  predVarNames = "tip_pred_1")
+
+
+
+pred <- rxPredict(model$two, pred, outData = mht$stage,
+                  writeModelVars = TRUE,
+                  predVarNames = "tip_pred_2")
+
+
+head(pred)
+# Q3 ----------------------------------------------------------------------
+histo <- rxHistogram(~ tip_percent, pred,
+                     rowSelection = (tip_percent < 50) & (tip_percent >0))
+
+
+# Q4 ----------------------------------------------------------------------
+
+histo <- rxHistogram(~ tip_pred_1, pred,
+                     rowSelection = (tip_percent < 50) & (tip_percent >0)
+                     # ,numBreaks = 20
+)
+
+histo <- rxHistogram(~ tip_pred_2, pred,
+                     rowSelection = (tip_percent < 50) & (tip_percent >0)
+                     # ,numBreaks = 20
+)
